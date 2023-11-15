da81075bc148d0b194c864f2b1aa4e0982e9d1eb
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-15 15:48:43 +0100

    yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :category, presence: true, inclusion: { in: %w(chinese italian japanese french belgian),
+                                                    message: '%{value} is not a valid category' }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Restaurant.destroy_all
+
+categories = %w(chinese italian japanese french belgian)
+
+5.times do
+  Restaurant.create(name: Faker::Company.name, address: Faker::Address.street_address,
+                    phone_number: Faker::PhoneNumber.phone_number, category: categories[rand(4)])
+end
+
+puts 'Seed generated !'
